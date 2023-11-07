800ddc3979e74778e6c05f225bad0159ccd584aa
zgljl2012 <zgljl2012@gmail.com>
2023-11-07 15:27:53 +0800

    VAE decoder

---
+++ src/vae/decoder.rs
@@ -0,0 +1,100 @@
+use tch::{nn::{self, Module}, Tensor};
+
+use crate::{unet::unet_2d_blocks::{UpDecoderBlock2D, UNetMidBlock2D, UNetMidBlock2DConfig, UpDecoderBlock2DConfig}, model_kind::ModelKind, utils};
+
+#[derive(Debug, Clone)]
+pub struct DecoderConfig {
+    // up_block_types: UpDecoderBlock2D
+    pub block_out_channels: Vec<i64>,
+    pub layers_per_block: i64,
+    pub norm_num_groups: i64,
+}
+
+impl Default for DecoderConfig {
+    fn default() -> Self {
+        Self { block_out_channels: vec![64], layers_per_block: 2, norm_num_groups: 32 }
+    }
+}
+
+#[derive(Debug)]
+pub struct Decoder {
+    conv_in: nn::Conv2D,
+    up_blocks: Vec<UpDecoderBlock2D>,
+    mid_block: UNetMidBlock2D,
+    conv_norm_out: nn::GroupNorm,
+    conv_out: nn::Conv2D,
+    #[allow(dead_code)]
+    config: DecoderConfig,
+}
+
+impl Decoder {
+    pub fn new(vs: nn::Path, in_channels: i64, out_channels: i64, config: DecoderConfig, base_model: ModelKind) -> Self {
+        let n_block_out_channels = config.block_out_channels.len();
+        let last_block_out_channels = *config.block_out_channels.last().unwrap();
+        let conv_cfg = nn::ConvConfig { stride: 1, padding: 1, ..Default::default() };
+        let conv_in =
+            nn::conv2d(&vs / "conv_in", in_channels, last_block_out_channels, 3, conv_cfg);
+        let mid_cfg = UNetMidBlock2DConfig {
+            resnet_eps: 1e-6,
+            output_scale_factor: 1.,
+            attn_num_head_channels: None,
+            resnet_groups: Some(config.norm_num_groups),
+            ..Default::default()
+        };
+        let mid_block =
+            UNetMidBlock2D::new(&vs / "mid_block", last_block_out_channels, None, mid_cfg, base_model);
+        let mut up_blocks = vec![];
+        let vs_up_blocks = &vs / "up_blocks";
+        let reversed_block_out_channels: Vec<_> =
+            config.block_out_channels.iter().copied().rev().collect();
+        for index in 0..n_block_out_channels {
+            let out_channels = reversed_block_out_channels[index];
+            let in_channels = if index > 0 {
+                reversed_block_out_channels[index - 1]
+            } else {
+                reversed_block_out_channels[0]
+            };
+            let is_final = index + 1 == n_block_out_channels;
+            let cfg = UpDecoderBlock2DConfig {
+                num_layers: config.layers_per_block + 1,
+                resnet_eps: 1e-6,
+                resnet_groups: config.norm_num_groups,
+                add_upsample: !is_final,
+                output_scale_factor: 1.,
+                ..Default::default()
+            };
+            let up_block =
+                UpDecoderBlock2D::new(&vs_up_blocks / index, in_channels, out_channels, cfg);
+            up_blocks.push(up_block)
+        }
+        let group_cfg = nn::GroupNormConfig { eps: 1e-6, ..Default::default() };
+        let conv_norm_out = nn::group_norm(
+            &vs / "conv_norm_out",
+            config.norm_num_groups,
+            config.block_out_channels[0],
+            group_cfg,
+        );
+        let conv_cfg = nn::ConvConfig { padding: 1, ..Default::default() };
+        let conv_out =
+            nn::conv2d(&vs / "conv_out", config.block_out_channels[0], out_channels, 3, conv_cfg);
+        Self { conv_in, up_blocks, mid_block, conv_norm_out, conv_out, config }
+    }
+}
+
+impl Module for Decoder {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        // 先卷积
+        let t = xs.apply(&self.conv_in);
+        // 经历 mid block
+        let mut xs = self.mid_block.forward(&t, None);
+        // 依次向上 block
+        let mut i = 0;
+        for up_block in self.up_blocks.iter() {
+            println!("--------------------------------------------------------------------------->>>{}, {}", i, utils::has_nan(&xs));
+            i += 1;
+            xs = xs.apply(up_block);
+        }
+        // 卷积且输出图像
+        xs.apply(&self.conv_norm_out).silu().apply(&self.conv_out)
+    }
+}
--- src/vae/mod.rs
+++ src/vae/mod.rs
@@ -3,192 +3,13 @@
 //! Auto-encoder models compress their input to a usually smaller latent space
 //! before expanding it back to its original shape. This results in the latent values
 //! compressing the original information.
-use crate::{unet::unet_2d_blocks::{
-    DownEncoderBlock2D, DownEncoderBlock2DConfig, UNetMidBlock2D, UNetMidBlock2DConfig,
-    UpDecoderBlock2D, UpDecoderBlock2DConfig}, model_kind::ModelKind};
-use tch::{nn, nn::Module, Tensor};
+use crate::model_kind::ModelKind;
+use tch::{nn, Tensor};
 
-#[derive(Debug, Clone)]
-struct EncoderConfig {
-    // down_block_types: DownEncoderBlock2D
-    block_out_channels: Vec<i64>,
-    layers_per_block: i64,
-    norm_num_groups: i64,
-    double_z: bool,
-}
+use self::{decoder::{Decoder, DecoderConfig}, encoder::{Encoder, EncoderConfig}};
 
-impl Default for EncoderConfig {
-    fn default() -> Self {
-        Self {
-            block_out_channels: vec![64],
-            layers_per_block: 2,
-            norm_num_groups: 32,
-            double_z: true,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Encoder {
-    conv_in: nn::Conv2D,
-    down_blocks: Vec<DownEncoderBlock2D>,
-    mid_block: UNetMidBlock2D,
-    conv_norm_out: nn::GroupNorm,
-    conv_out: nn::Conv2D,
-    #[allow(dead_code)]
-    config: EncoderConfig,
-}
-
-impl Encoder {
-    fn new(vs: nn::Path, in_channels: i64, out_channels: i64, config: EncoderConfig, base_model: ModelKind) -> Self {
-        let conv_cfg = nn::ConvConfig { stride: 1, padding: 1, ..Default::default() };
-        let conv_in =
-            nn::conv2d(&vs / "conv_in", in_channels, config.block_out_channels[0], 3, conv_cfg);
-        let mut down_blocks = vec![];
-        let vs_down_blocks = &vs / "down_blocks";
-        for index in 0..config.block_out_channels.len() {
-            let out_channels = config.block_out_channels[index];
-            let in_channels = if index > 0 {
-                config.block_out_channels[index - 1]
-            } else {
-                config.block_out_channels[0]
-            };
-            let is_final = index + 1 == config.block_out_channels.len();
-            let cfg = DownEncoderBlock2DConfig {
-                num_layers: config.layers_per_block,
-                resnet_eps: 1e-6,
-                resnet_groups: config.norm_num_groups,
-                add_downsample: !is_final,
-                downsample_padding: 0,
-                ..Default::default()
-            };
-            let down_block =
-                DownEncoderBlock2D::new(&vs_down_blocks / index, in_channels, out_channels, cfg);
-            down_blocks.push(down_block)
-        }
-        let last_block_out_channels = *config.block_out_channels.last().unwrap();
-        let mid_cfg = UNetMidBlock2DConfig {
-            resnet_eps: 1e-6,
-            output_scale_factor: 1.,
-            attn_num_head_channels: None,
-            resnet_groups: Some(config.norm_num_groups),
-            ..Default::default()
-        };
-        let mid_block =
-            UNetMidBlock2D::new(&vs / "mid_block", last_block_out_channels, None, mid_cfg, base_model);
-        let group_cfg = nn::GroupNormConfig { eps: 1e-6, ..Default::default() };
-        let conv_norm_out = nn::group_norm(
-            &vs / "conv_norm_out",
-            config.norm_num_groups,
-            last_block_out_channels,
-            group_cfg,
-        );
-        let conv_out_channels = if config.double_z { 2 * out_channels } else { out_channels };
-        let conv_cfg = nn::ConvConfig { padding: 1, ..Default::default() };
-        let conv_out =
-            nn::conv2d(&vs / "conv_out", last_block_out_channels, conv_out_channels, 3, conv_cfg);
-        Self { conv_in, down_blocks, mid_block, conv_norm_out, conv_out, config }
-    }
-}
-
-impl Module for Encoder {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        let mut xs = xs.apply(&self.conv_in);
-        for down_block in self.down_blocks.iter() {
-            xs = xs.apply(down_block)
-        }
-        self.mid_block.forward(&xs, None).apply(&self.conv_norm_out).silu().apply(&self.conv_out)
-    }
-}
-
-#[derive(Debug, Clone)]
-struct DecoderConfig {
-    // up_block_types: UpDecoderBlock2D
-    block_out_channels: Vec<i64>,
-    layers_per_block: i64,
-    norm_num_groups: i64,
-}
-
-impl Default for DecoderConfig {
-    fn default() -> Self {
-        Self { block_out_channels: vec![64], layers_per_block: 2, norm_num_groups: 32 }
-    }
-}
-
-#[derive(Debug)]
-struct Decoder {
-    conv_in: nn::Conv2D,
-    up_blocks: Vec<UpDecoderBlock2D>,
-    mid_block: UNetMidBlock2D,
-    conv_norm_out: nn::GroupNorm,
-    conv_out: nn::Conv2D,
-    #[allow(dead_code)]
-    config: DecoderConfig,
-}
-
-impl Decoder {
-    fn new(vs: nn::Path, in_channels: i64, out_channels: i64, config: DecoderConfig, base_model: ModelKind) -> Self {
-        let n_block_out_channels = config.block_out_channels.len();
-        let last_block_out_channels = *config.block_out_channels.last().unwrap();
-        let conv_cfg = nn::ConvConfig { stride: 1, padding: 1, ..Default::default() };
-        let conv_in =
-            nn::conv2d(&vs / "conv_in", in_channels, last_block_out_channels, 3, conv_cfg);
-        let mid_cfg = UNetMidBlock2DConfig {
-            resnet_eps: 1e-6,
-            output_scale_factor: 1.,
-            attn_num_head_channels: None,
-            resnet_groups: Some(config.norm_num_groups),
-            ..Default::default()
-        };
-        let mid_block =
-            UNetMidBlock2D::new(&vs / "mid_block", last_block_out_channels, None, mid_cfg, base_model);
-        let mut up_blocks = vec![];
-        let vs_up_blocks = &vs / "up_blocks";
-        let reversed_block_out_channels: Vec<_> =
-            config.block_out_channels.iter().copied().rev().collect();
-        for index in 0..n_block_out_channels {
-            let out_channels = reversed_block_out_channels[index];
-            let in_channels = if index > 0 {
-                reversed_block_out_channels[index - 1]
-            } else {
-                reversed_block_out_channels[0]
-            };
-            let is_final = index + 1 == n_block_out_channels;
-            let cfg = UpDecoderBlock2DConfig {
-                num_layers: config.layers_per_block + 1,
-                resnet_eps: 1e-6,
-                resnet_groups: config.norm_num_groups,
-                add_upsample: !is_final,
-                ..Default::default()
-            };
-            let up_block =
-                UpDecoderBlock2D::new(&vs_up_blocks / index, in_channels, out_channels, cfg);
-            up_blocks.push(up_block)
-        }
-        let group_cfg = nn::GroupNormConfig { eps: 1e-6, ..Default::default() };
-        let conv_norm_out = nn::group_norm(
-            &vs / "conv_norm_out",
-            config.norm_num_groups,
-            config.block_out_channels[0],
-            group_cfg,
-        );
-        let conv_cfg = nn::ConvConfig { padding: 1, ..Default::default() };
-        let conv_out =
-            nn::conv2d(&vs / "conv_out", config.block_out_channels[0], out_channels, 3, conv_cfg);
-        Self { conv_in, up_blocks, mid_block, conv_norm_out, conv_out, config }
-    }
-}
-
-impl Module for Decoder {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        let t = xs.apply(&self.conv_in);
-        let mut xs = self.mid_block.forward(&t, None);
-        for up_block in self.up_blocks.iter() {
-            xs = xs.apply(up_block)
-        }
-        xs.apply(&self.conv_norm_out).silu().apply(&self.conv_out)
-    }
-}
+mod decoder;
+mod encoder;
 
 #[derive(Debug, Clone)]
 pub struct AutoEncoderKLConfig {
@@ -251,6 +72,7 @@ impl AutoEncoderKL {
         config: AutoEncoderKLConfig,
         base_model: ModelKind
     ) -> Self {
+        println!("----->>>>>999 {}", config.layers_per_block);
         let latent_channels = config.latent_channels;
         let encoder_cfg = EncoderConfig {
             block_out_channels: config.block_out_channels.clone(),
